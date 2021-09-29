6643d754fb0aef00826cb6c8cd6d15fdb995763c
Jay Prakash <unijay12@gmail.com>
2021-09-29 16:54:17 +0530

    static root added

--- core/settings.py
+++ core/settings.py
@@ -165,7 +165,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=BASE_DIR/'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
